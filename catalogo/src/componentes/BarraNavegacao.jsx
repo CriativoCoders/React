5bbbb059d4289712a6ad4a45c85c3109aa211dbb
Talita Cristina Alves Lobato <05058225204@>
2025-03-25 11:15:45 -0300

    primeiros passos react

---
+++ catalogo/src/componentes/BarraNavegacao.jsx
@@ -0,0 +1,13 @@
+import estilos from './BarraNavegacao.module.css';
+
+export function BarraNavegacao(){
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Filmes</li>
+                <li>Perfil</li>
+            </ul>
+        </nav>
+    )
+}
---
+++ catalogo/src/componentes/Conteudo.jsx
@@ -0,0 +1,10 @@
+import estilos from './Conteudo.module.css';
+
+
+export function Conteudo(){
+    return(
+        <main className={estilos.container}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic molestiae vitae quos minima fuga odit nesciunt ad facilis quod, rerum, voluptates voluptas. Error nam deserunt obcaecati atque magni eveniet iste.
+        </main>
+    )
+}
